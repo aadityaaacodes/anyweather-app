0bf52dc93d8939ce149f60c4ee67d68aa7d6a360
Aaditya Sawant <aadityaaacodes@gmail.com>
2024-10-27 00:46:28 -0400

    Added routes and functions to Flask App

--- app.py
+++ app.py
@@ -1,7 +1,42 @@
 from flask import Flask, render_template, request, redirect, url_for
+from extractor import getInfo, performGoogleSearch, putInfo
 
 app = Flask(__name__)
 
+@app.route('/', methods=['GET', 'POST'])
+def default():
+    if request.method == 'GET':
+        x = getInfo()
+        searchdata = x['searchData']
+        weatherData = x['weatherData']
+        imageLibrary = {
+            "Sunny" : "https://i.gifer.com/Iqp.gif", 
+            "Windy" : "https://i.gifer.com/fzmZ.gif",
+            "Storm" : "https://i.gifer.com/604.gif", 
+            "Snow" : "https://i.gifer.com/3gF.gif",
+            "Hail" : "https://i.gifer.com/Cba.gif",
+            "Cloudy" : "https://i.gifer.com/srG.gif", 
+            "Rain" : "https://i.gifer.com/AcU9.gif",
+            "Haze" : "https://i.gifer.com/7Z6Q.gif",
+            None : "" 
+        }
+
+        url = ""
+
+        i = (weatherData['condition']).split(' ')
+        for j in imageLibrary:
+            if j in i:
+                url = imageLibrary[f'{j}']
+
+        return(render_template('weather.html', Img_URL=url, info=weatherData, city=searchdata['city']))
+    elif request.method == 'POST':
+        query = request.form.get('query')
+        x = getInfo()
+        searchData = x['searchData']
+        searchData['city'] = query
+        putInfo(x)
+        performGoogleSearch(query=f"{query}")
+        return (redirect(url_for('default')))
 
 
 if __name__ == '__main__':
